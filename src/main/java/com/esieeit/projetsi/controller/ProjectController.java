01c4631e6a96dca36df214fcedc9bc95fa2e9d67
Amin <aminaghani05@gmail.com>
2026-02-26 09:27:19 +0100

    feat(api): mise en place des DTOs et de la structure controller (TP 3.1 & 3.2)

---
+++ src/main/java/com/esieeit/projetsi/controller/ProjectController.java
@@ -0,0 +1,12 @@
+package com.esieeit.projetsi.controller;
+
+import com.esieeit.projetsi.api.dto.ProjectDTO;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectController {
+    // Cette méthode simulera plus tard une réponse API
+    public List<ProjectDTO> getAllProjects() {
+        return new ArrayList<>(); 
+    }
+}
